dd23a720b6bc203d420917cd8c7e9f961286dc87
David Contreras <davidcb@comillas.edu>
2019-11-06 09:31:43 +0100

    Cloudant

--- src/main/java/asr/proyectoFinal/servlets/Controller.java
+++ src/main/java/asr/proyectoFinal/servlets/Controller.java
@@ -30,7 +30,7 @@ public class Controller extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
-		PrintWriter out = response.getWriter();
+		PrintWriter out = response.getWriter(); 
 		out.println("<html><head><meta charset=\"UTF-8\"></head><body>");
 		
 		CloudantPalabraStore store = new CloudantPalabraStore();
