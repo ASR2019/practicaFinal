ffd1a9dc724700d06e496b6759c03869073501d0
pmurcia <p.murcia.morilla@gmail.com>
2019-11-18 17:16:08 +0100

    Commented unused imported packages

--- src/main/java/asr/proyectoFinal/servlets/Controller.java
+++ src/main/java/asr/proyectoFinal/servlets/Controller.java
@@ -1,16 +1,16 @@
 package asr.proyectoFinal.servlets;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
+//import java.io.BufferedWriter;
+//import java.io.File;
+//import java.io.FileOutputStream;
+//import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+//import java.io.InputStream;
+//import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.nio.Buffer;
-import java.nio.file.Files;
-import java.util.List;
+//import java.nio.Buffer;
+//import java.nio.file.Files;
+//import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
